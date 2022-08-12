aa1fe5b3fbdf12e9d1f4de39d524dadaf114e4fc
DerputeR <dekxy-developer@yahoo.com>
2022-08-12 16:20:23 -0400

    svg text experiments

--- js/index.js
+++ js/index.js
@@ -12,42 +12,72 @@ const io = document.body.querySelector("#io");
 const b_debug_render = io.querySelector("#b_debug_render");
 const debug_render_url = io.querySelector("#debug_render_url");
 let render_url = "";
+let file_name = "lineup.png";
 
-b_debug_render.addEventListener("click", render);
+const export_anchor = io.querySelector("#download_export");
+const b_export = io.querySelector("#b_export");
+
+const preview_height = 300;
 
 function render() {
-    // briefly show element for render
-    prerender_container.style.display = "block";
-    // prerender_container.style.opacity = "0";
+    return new Promise((resolve, reject) => {
+        // briefly show element for render
+        prerender_container.style.display = "block";
+        // prerender_container.style.opacity = "0";
 
-    // set appropriate dimensions
-    let c_width = prerender_div.offsetWidth;
-    let c_height = prerender_div.offsetHeight;
-    console.log(`${c_width} x ${c_height}`);
+        // set appropriate dimensions
+        let c_width = prerender_div.offsetWidth;
+        let c_height = prerender_div.offsetHeight;
+        console.log(`${c_width} x ${c_height}`);
 
-    // clear canvas and update size
-    render_canvas_ctx.clearRect(0, 0, render_canvas.width, render_canvas.height);
-    render_canvas.width = c_width;
-    render_canvas.height = c_height;
+        // clear canvas and update size
+        let dpi = window.devicePixelRatio;
+        dpi = 1; // hard lock scaling to preserve original resolution
+        render_canvas_ctx.clearRect(0, 0, render_canvas.width, render_canvas.height);
+        render_canvas.width = c_width * dpi;
+        render_canvas.height = c_height * dpi;
+        render_canvas.style.width = c_width * (preview_height / c_height) + "px";
+        render_canvas.style.height = preview_height + "px";
+        render_canvas_ctx.imageSmoothing = false;
 
-    html2canvas(prerender_div, {
-        backgroundColor: null,
-        canvas: render_canvas,
-        scale: 1
-    }).then(function() {
-        // hide composite div when done
-        // prerender_container.style.display = "none";
-        
-        // reset the transform when done since html2canvas forgets about this
-        render_canvas_ctx.setTransform(1,0,0,1,0,0)
-        
-        // prepare the render for download
-        const blob = render_canvas.toBlob((blob) => {
-            URL.revokeObjectURL(render_url);
-            render_url = URL.createObjectURL(blob);
-            debug_render_url.value = render_url;
-        });
-    }); 
+        html2canvas(prerender_div, {
+            backgroundColor: null,
+            canvas: render_canvas,
+            scale: dpi,
+            width: c_width,
+            height: c_height
+        }).then(function () {
+            // hide composite div when done
+            // prerender_container.style.display = "none";
+            
+            // reset the transform when done since html2canvas forgets about this
+            render_canvas_ctx.setTransform(1, 0, 0, 1, 0, 0)
+            
+            // prepare the render for download
+            const blob = render_canvas.toBlob((blob) => {
+                URL.revokeObjectURL(render_url);
+                render_url = URL.createObjectURL(blob);
+                debug_render_url.value = render_url;
+            });
+            resolve();
+        }); 
+    });
 }
 
+function exportRender(url, file_name, export_anchor) {
+    export_anchor.setAttribute("href", url);
+    export_anchor.setAttribute("download", file_name);
+    export_anchor.click();
+}
 
+b_debug_render.addEventListener("click", (event) => {
+    render();
+});
+b_export.addEventListener("click", (event) => {
+    if (!render_url) {
+        const r = render();
+        r.then(exportRender(render_url, file_name, export_anchor));
+    } else {
+        exportRender(render_url, file_name, export_anchor);
+    }
+});
