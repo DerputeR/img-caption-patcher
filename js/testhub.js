62a3e614fce2c01d749f34eb6d729217237f9b1c
DerputeR <dekxy-developer@yahoo.com>
2022-08-14 02:19:07 -0400

    wasted time trying to get zooming to work
    I could have finished the entire playbook nades by hand already

--- js/testhub.js
+++ js/testhub.js
@@ -12,6 +12,8 @@ const editor_frame = editor_image.querySelector(".frame");
 const zoom = editor.querySelector("#zoom");
 const zoom_slider = zoom.querySelector("#zoom-slider");
 const zoom_number = zoom.querySelector("#zoom-num");
+const b_zoom_fit = zoom.querySelector("#zoom-fit");
+const debug_anchor = editor.querySelector(".anchor");
 let zoom_percentage = 100;
 
 /// Render preview
@@ -101,10 +103,11 @@ b_export.addEventListener("click", (event) => {
 
 
 
-// 0 = slider moved, 1 = number changed, 2 = scroll-wheeled
+// 0 = slider moved, 1 = number changed, 2 = scroll-wheeled, 3 = force zoom
 // positive scroll_count for scroll up, negative for scroll down
-function updateZoom(mode = 0, scroll_count = 0) {
-    mode = mode % 3;
+function updateZoom(mode = 0, scroll_count = 0, anchor_x = 0, anchor_y = 0, zoom_override = 100) {
+    let last_zoom = zoom_percentage;
+    mode = mode % 4;
     switch (mode) {
         case 0:
             console.log("zoom slider updated");
@@ -122,44 +125,101 @@ function updateZoom(mode = 0, scroll_count = 0) {
             zoom_slider.number = zoom_percentage;
             zoom_slider.value = zoom_percentage;
             break;
+        case 3:
+            console.log("zoom via direct command");
+            zoom_percentage = zoom_override;
+            zoom_number.value = zoom_percentage;
+            zoom_slider.value = zoom_percentage;
+            break;
         default:
             zoom_percentage = 100;
             break;
     
     }
 
-    console.log(`Pre-zoom scroll pos: (${editor_image.scrollLeft}, ${editor_image.scrollTop})`);
+    // since origin (0,0) is at top-left, we must do something like this:
+    // image coords and viewport coordinates are different
+    // we must convert viewport coordinates to image coordinates,
+    // apply the scaling to this coordinates, then determine the change in these coordinates
+    // WE WILL NOT CONVERT BACK TO VIEWPORT COORDINATES since we do not need them to find the change in position of the anchor point after scaling
 
+    let originX = -editor_image.scrollLeft;
+    let originY = -editor_image.scrollTop;
+    console.log(anchor_x);
+
+    //! This isn't working. Debug; if cannot figure this out, resort to
+    //! doing one-way scales, but doing (zoom_percentage - last_zoom) + 100 as the scale parameter
+    // undo previous translate and scaling
+    // let undoDeltas = getDeltaToMoveScaleAboutAnchor(anchor_x, anchor_y, originX, originY, (100 / last_zoom) * 100);
+
+    // let unscaledAnchorX = anchor_x + undoDeltas[0];
+    // let unscaledAnchorY = anchor_y + undoDeltas[1];
+    // let unscaledOriginX = originX + undoDeltas[0];
+    // let unscaledOriginY = originY + undoDeltas[1];
+    // console.log(unscaledAnchorX);
+
+    // // apply new scaling
+    // let newDeltas = getDeltaToMoveScaleAboutAnchor(unscaledAnchorX, unscaledAnchorY, unscaledOriginX, unscaledOriginY, zoom_percentage);
+
+    // // translate
+    // let movedAnchorX = unscaledAnchorX + newDeltas[0];
+    // let movedAnchorY = unscaledAnchorY + newDeltas[1];
+    // console.log(movedAnchorX);
+
+    // // Compute the total delta
+    // let dx = movedAnchorX - anchor_x;
+    // let dy = movedAnchorY - anchor_y;
+    // console.log(dx);
+
+    //! WHY DOESN'T THIS WORK??? IS MY ORIGIN WRONG???
+    let scaleBy = (zoom_percentage - last_zoom) + 100;
+    let deltas = getDeltaToMoveScaleAboutAnchor(anchor_x, anchor_y, originX, originY, scaleBy);
+    let dx = deltas[0];
+    let dy = deltas[1];
     
     editor_frame.style.transform = `scale(${zoom_percentage}%)`;
     editor_frame_wrap.style.width = `${editor_frame.scrollWidth * (zoom_percentage / 100)}px`;
     editor_frame_wrap.style.height = `${editor_frame.scrollHeight * (zoom_percentage / 100)}px`;
-    
-    let lastLeft = editor_image.scrollLeft;
-    let lastTop = editor_image.scrollTop;
 
-    console.log(`Post-zoom scroll pos: (${editor_image.scrollLeft}, ${editor_image.scrollTop})`);
-
-    let barWidth = editor_image.scrollWidth - editor_image.clientWidth;
-    let barHeight = editor_image.scrollHeight - editor_image.clientHeight;
 
     if (editor_image.scrollWidth > editor_image.clientWidth) {
-        let neededLeft = ((editor_image.scrollWidth - editor_image.clientWidth) / 2);
-        editor_image.scrollLeft = neededLeft;
+        editor_image.scrollLeft += dx;
     }
     if (editor_image.scrollHeight > editor_image.clientHeight) {
-        let neededTop = ((editor_image.scrollHeight - editor_image.clientHeight) / 2);
-        editor_image.scrollTop = neededTop;
+        editor_image.scrollTop += dy;
     }
-
-    console.log(`Centered scroll pos: (${editor_image.scrollLeft}, ${editor_image.scrollTop})`);
 }
+
 zoom_slider.addEventListener("input", (event) => {
-    updateZoom(0);
+    // updateZoom(0, undefined, (editor_image.clientWidth / 2), (editor_image.clientHeight / 2));
+    updateZoom(0, undefined, 0, 0);
 });
 zoom_number.addEventListener("change", (event) => {
-    updateZoom(1);
+    updateZoom(1, undefined, editor_image.offsetWidth / 2, editor_image.offsetHeight / 2);
 });
 
+b_zoom_fit.addEventListener("click", (event) => {
+    let x_percent = editor_image.clientWidth / editor_frame.scrollWidth;
+    let y_percent = editor_image.clientHeight / editor_frame.scrollHeight;
+    let fit = Math.min(x_percent, y_percent) * 100;
+    console.log(`fitting at ${fit}%`);
+    updateZoom(3, undefined, 0, 0, fit);
+})
+
 // page load stuff
+debug_anchor.style.width = `${editor_image.clientWidth}px`;
+debug_anchor.style.height = `${editor_image.clientHeight}px`;
+
 updateZoom(0);
+
+//* scaleByPercent: 100 = 100%
+// based on my thing here: https://www.desmos.com/calculator/0xtrac7und
+function getDeltaToMoveScaleAboutAnchor(anchorX, anchorY, scaleOriginX, scaleOriginY, scaleByPercent) {
+    let scaledAtOriginX = (scaleByPercent * .01) * (anchorX - scaleOriginX) + scaleOriginX;
+    let scaledAtOriginY = (scaleByPercent * .01) * (anchorY - scaleOriginY) + scaleOriginY;
+
+    let deltaX = scaledAtOriginX - anchorX;
+    let deltaY = scaledAtOriginY - anchorY;
+
+    return [deltaX, deltaY];
+}
